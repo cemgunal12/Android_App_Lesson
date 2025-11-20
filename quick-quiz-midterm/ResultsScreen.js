cefab766218cb1740f6e4e593f5b9e2fb49d569c
Cem Günal <126788995+cemgunal12@users.noreply.github.com>
2025-11-20 13:59:52 +0300

    Add files via upload

---
+++ quick-quiz-midterm/ResultsScreen.js
@@ -0,0 +1,15 @@
+// src/ResultsScreen.js
+
+function ResultsScreen({ score, totalQuestions, onRestart }) {
+  return (
+    <div className="results-screen">
+      <h2>Quiz Finished!</h2>
+      <p>You scored {score} out of {totalQuestions}.</p>
+          <button onClick={onRestart} className="restart-btn">
+        Restart Quiz
+      </button>
+    </div>
+  );
+}
+
+export default ResultsScreen;
