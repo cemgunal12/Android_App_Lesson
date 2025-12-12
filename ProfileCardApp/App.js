7db9921dbfff0b341698cf9c9978b3a9b0c29643
cemgunal12 <126788995+cemgunal12@users.noreply.github.com>
2025-12-12 16:14:25 +0300

    create theme system with design tokens

--- ProfileCardApp/App.js
+++ ProfileCardApp/App.js
@@ -1,22 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
+import ProfileScreen from './screens/ProfileScreen';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     'Montserrat': require('./assets/fonts/Montserrat-Regular.ttf'),
     'MontserratBold': require('./assets/fonts/Montserrat-Bold.ttf'),
   });
+
   if (!fontsLoaded) {
     return null;
   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Fonts Loaded!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <ProfileScreen />;
 }
 
 const styles = StyleSheet.create({
