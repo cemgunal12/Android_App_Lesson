9c25fd32be18b78f534e45bc0233767c367c847e
cemgunal12 <126788995+cemgunal12@users.noreply.github.com>
2025-12-26 16:57:33 +0300

    feat: basic login using local state

--- PreferencesApp/screens/HomeScreen.js
+++ PreferencesApp/screens/HomeScreen.js
@@ -1,13 +1,23 @@
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function HomeScreen() {
+export default function HomeScreen({ route }) {
+  const { username } = route.params; // Gelen parametreyi al
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text style={styles.text}>Welcome, {username}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 });
